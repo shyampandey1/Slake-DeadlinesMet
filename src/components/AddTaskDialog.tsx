5128d313f3eb0bbbe6cd5ba595fb48d99d24b28a
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-07-28 18:17:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/AddTaskDialog.tsx
+++ src/components/AddTaskDialog.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -78,7 +78,7 @@ export default function AddTaskDialog({ isOpen, onClose, onAddTask, initialCateg
     },
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (isOpen) {
       form.reset({
         taskName: "",
