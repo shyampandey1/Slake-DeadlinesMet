bdc68949116e614803e89bf8a32eae76d04f2faf
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-07-30 20:39:22 +0000

    the webapp install popup did not come in my phone when I tested

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Orbitron:wght@700&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
