96623e4deadb1ed22d873e8cb5a0b40fcdd2ef62
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-08-02 06:37:50 +0000

    fix layout for this sliding arrow, keep it only for desktop and the arro

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -207,9 +207,9 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full hidden md:block",
+        "absolute z-10 h-10 w-10 rounded-full bg-background/50 hover:bg-background/80 items-center justify-center hidden md:flex",
         orientation === "horizontal"
-          ? "left-0 top-1/2 -translate-y-1/2"
+          ? "left-4 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -217,7 +217,7 @@ const CarouselPrevious = React.forwardRef<
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft className="h-4 w-4" />
+      <ArrowLeft className="h-5 w-5" />
       <span className="sr-only">Previous slide</span>
     </Button>
   )
@@ -236,9 +236,9 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 rounded-full hidden md:block",
+        "absolute z-10 h-10 w-10 rounded-full bg-background/50 hover:bg-background/80 items-center justify-center hidden md:flex",
         orientation === "horizontal"
-          ? "right-0 top-1/2 -translate-y-1/2"
+          ? "right-4 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
@@ -246,7 +246,7 @@ const CarouselNext = React.forwardRef<
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight className="h-4 w-4" />
+      <ArrowRight className="h-5 w-5" />
       <span className="sr-only">Next slide</span>
     </Button>
   )
