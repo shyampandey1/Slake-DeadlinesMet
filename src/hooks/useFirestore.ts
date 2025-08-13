17c602a5b499e965e210b96d6da1b0604de2aa57
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-08-13 07:57:15 +0000

    short walk is added for logged in account, but how would people finish t

--- src/hooks/useFirestore.ts
+++ src/hooks/useFirestore.ts
@@ -131,7 +131,7 @@ export function useTasks() {
 }
 
 // Version for the default routines data structure
-const ROUTINE_TEMPLATE_VERSION = 19;
+const ROUTINE_TEMPLATE_VERSION = 20;
 
 const creativeProfessionalRoutine: Omit<UserPresetTask, "id" | "order" | "profession">[] = [
     { name: "Drink a glass of water", duration: 1, icon: "Droplets", category: "Morning Routine" },
