b262ca0b818d3c6b4cf0f4e928784c8db2d78234
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-08-14 18:07:18 +0000

    clicking the task finished notification did not bring that completion sc

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,27 +1,38 @@
 
-self.addEventListener('install', (event) => {
-  console.log('Service Worker installing.');
-});
+// A simple, no-op service worker that exists to allow us to display
+// web-based notifications.
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker activating.');
+self.addEventListener('install', (event) => {
+    // This is a no-op, but it's good practice to have an install event listener.
+    // It can be used for pre-caching assets in a more advanced PWA.
 });
 
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
 
+  const urlToOpen = new URL(self.location.origin).href;
+
   event.waitUntil(
-    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+    clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true,
+    }).then((clientList) => {
       if (clientList.length > 0) {
         let client = clientList[0];
+        // Try to find a focused client first.
         for (let i = 0; i < clientList.length; i++) {
           if (clientList[i].focused) {
             client = clientList[i];
+            break;
           }
         }
-        return client.focus();
+        // If we found a client, focus it.
+        if (client) {
+            return client.focus();
+        }
       }
-      return clients.openWindow('/');
+      // If no client is found or focused, open a new window.
+      return clients.openWindow(urlToOpen);
     })
   );
 });
