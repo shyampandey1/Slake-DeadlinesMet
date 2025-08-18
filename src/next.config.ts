22297260a7caea0a4e5f3553ee5601f3204bf7a9
Shyam Pandey (Sam) <shyamp028@gmail.com>
2025-08-18 09:18:52 +0000

    can you check and verify if we are ready to publish this app on firebase

--- src/next.config.ts
+++ src/next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -17,6 +18,14 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    // Ignore warnings for handlebars
+    config.module.rules.push({
+      test: /node_modules\/handlebars\/lib\/index\.js$/,
+      loader: 'ignore-loader',
+    });
+    return config;
   },
 };
 
